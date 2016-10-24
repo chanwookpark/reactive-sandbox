e1c95c6675f8d702aceb37b97203a5c353f9b016
chanwook <chanwook.god@gmail.com>
2016-10-24 23:33:03 +0900

    안쪽까지 따라 테스트하는 건 일단 실패.. 지금하기에는 난이도가 너무 높다 ㅠㅠ

--- src/test/java/com/github/chanwookpark/literx/Step1Test.java
+++ src/test/java/com/github/chanwookpark/literx/Step1Test.java
@@ -8,6 +8,11 @@ import reactor.core.publisher.Flux;
  */
 public class Step1Test {
 
+    /**
+     * Part01CreateFlux#empty
+     *
+     * @throws Exception
+     */
     @Test
     public void createEmptyFlux() throws Exception {
 
@@ -16,5 +21,16 @@ public class Step1Test {
                 .subscribe(empty)
                 .assertValueCount(0)
                 .assertComplete();
+    }
+
+    @Test
+    public void fromValues() throws Exception {
+        final Flux<String> flux = Flux.just("foo", "bar");
+        TestSubscriber
+                .subscribe(flux)
+                .assertValues("foo", "bar")
+                .assertComplete();
+
+
     }
 }
--- src/test/java/com/github/chanwookpark/literx/TestSubscriber.java
+++ src/test/java/com/github/chanwookpark/literx/TestSubscriber.java
@@ -3,6 +3,11 @@ package com.github.chanwookpark.literx;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Operators;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 /**
  * @author chanwook
@@ -12,6 +17,20 @@ public class TestSubscriber<T> implements Subscriber<T> {
     private volatile int valueCount = 0;
 
     private int completionCount = 0;
+
+    private int subscriptionCount;
+
+    volatile long requested;
+
+    volatile List<T> values = new LinkedList<>();
+
+    volatile Subscription subscription;
+
+    private static final AtomicReferenceFieldUpdater<TestSubscriber, Subscription> S =
+            AtomicReferenceFieldUpdater.newUpdater(TestSubscriber.class, Subscription.class, "subscription");
+
+    private static final AtomicLongFieldUpdater<TestSubscriber> REQUESTED =
+            AtomicLongFieldUpdater.newUpdater(TestSubscriber.class, "requested");
 
     /**
      * 전달 받은 Flux가 publisher고, 이 publisher의 subscriber로 TestSubscriber 인스턴스를 등록
@@ -28,7 +47,31 @@ public class TestSubscriber<T> implements Subscriber<T> {
 
     @Override
     public void onSubscribe(Subscription subscription) {
+        subscriptionCount++;
 
+        //FIXME 도대체 아래 로직은 왜 하는걸까..?
+        Subscription s = this.subscription;
+        if (Operators.cancelledSubscription() == s) {
+            subscription.cancel();
+            postCancel(subscription);
+        } else if (s != null) {
+            subscription.cancel();
+            Operators.reportSubscriptionSet();
+            postCancel(subscription);
+        }
+
+        if (S.compareAndSet(this, null, subscription)) {
+            long r = REQUESTED.getAndSet(this, 0L);
+
+            if (r != 0L) {
+                subscription.request(r);
+            }
+
+        }
+    }
+
+    private void postCancel(Subscription subscription) {
+        //TODO
     }
 
     @Override
@@ -48,7 +91,7 @@ public class TestSubscriber<T> implements Subscriber<T> {
 
     public TestSubscriber<T> assertValueCount(int expected) {
         if (valueCount != expected) {
-            throw new AssertionError("Difference value count (real: " + valueCount + ", expected: " + expected + ")");
+            throw new AssertionError("Difference value count (actual: " + valueCount + ", expected: " + expected + ")");
         }
         return this;
     }
@@ -61,6 +104,27 @@ public class TestSubscriber<T> implements Subscriber<T> {
         } else if (currentCompletionCount > 1) {
             throw new AssertionError("Multiple completions! (count: " + currentCompletionCount + ")");
         }
+        return this;
+    }
+
+    public TestSubscriber<T> assertValues(T... expectedValues) {
+
+        if (expectedValues.length != values.size()) {
+            throw new AssertionError("Not equals of values (expected size: " + expectedValues.length + ", actual size: " + values.size() + ")");
+        }
+
+        Iterator<T> actualIterator = values.iterator();
+        final Iterator<T> expectedIterator = Arrays.asList(expectedValues).iterator();
+
+        while (actualIterator.hasNext()) {
+            T actual = actualIterator.next();
+            T expected = expectedIterator.next();
+
+            if (!Objects.equals(actual, expected)) {
+                throw new AssertionError("Value does not equals! (expected: " + expected + ", actual: " + actual + ")");
+            }
+        }
+
         return this;
     }
 }
